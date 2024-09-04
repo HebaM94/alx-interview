13b51954e0bf2349a439fc564cffc9116d8cef5a
Heba <hebamagde@gmail.com>
2024-09-04 03:01:44 +0300

    update semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,28 @@
 #!/usr/bin/node
 
 function makeRequest (url) {
-    const request = require('request');
-    return new Promise((resolve, reject) => {
-      request.get(url, (error, response, body) => {
-        if (error) reject(error);
-        else resolve(JSON.parse(body));
-      });
+  const request = require('request');
+  return new Promise((resolve, reject) => {
+    request.get(url, (error, response, body) => {
+      if (error) reject(error);
+      else resolve(JSON.parse(body));
     });
+  });
+}
+
+async function main () {
+  const args = process.argv;
+
+  if (args.length < 3) return;
+
+  const movieUrl = 'https://swapi-api.alx-tools.com/api/films/' + args[2];
+  const movie = await makeRequest(movieUrl);
+
+  if (movie.characters === undefined) return;
+  for (const characterUrl of movie.characters) {
+    const character = await makeRequest(characterUrl);
+    console.log(character.name);
   }
-  
-  async function main () {
-    const args = process.argv;
-  
-    if (args.length < 3) return;
-  
-    const movieUrl = 'https://swapi-api.alx-tools.com/api/films/' + args[2];
-    const movie = await makeRequest(movieUrl);
-  
-    if (movie.characters === undefined) return;
-    for (const characterUrl of movie.characters) {
-      const character = await makeRequest(characterUrl);
-      console.log(character.name);
-    }
-  }
-  
-  main();
+}
+
+main();
