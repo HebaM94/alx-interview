38ca56b96c209f766735bec85952e816b3098152
Heba <hebamagde@gmail.com>
2024-09-12 20:23:24 +0300

    update pycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,12 +3,13 @@
 
 
 def rotate_2d_matrix(matrix):
-    """Rotate 
+    """Rotate n x n 2D matrix 90 decrease clockwise
+        in place
     """
     n = len(matrix)
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
+
     for i in range(n):
         matrix[i].reverse()
